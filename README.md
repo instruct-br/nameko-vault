aa1c96c1cb0c0c3469a529a809cfaf886e1196e7
fabio <fabioo.salgado@gmail.com>
2020-05-29 13:16:05 -0300

    refactor(vault): Refactor method get_key_value

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ To use this tool it is necessary to configure the following parameters in your n
 VAULT_URL: <vault_api_url>
 VAULT_TOKEN: <authentication_token>
 ```
+
+Up to the present moment there is only support for the key_value backend, to use it just inform the path where the keys are located as in the example below:
+
+```
+vault = VaultProvider()
+vault.get_kv_secret(mount_point="example", path="path/test")
+```
--- nameko_vault/__init__.py
+++ nameko_vault/__init__.py
@@ -6,16 +6,23 @@ class VaultProvider(DependencyProvider):
     def setup(self):
         url = self.container.config.get("VAULT_URL", "")
         self.client = hvac.Client(url=url)
-
-    def get_dependency(self, worker_ctx):
         if self.client.is_sealed():
-            keys = worker_ctx.container.config.get("VAULT_KEYS", "")
+            keys = self.container.config.get("VAULT_KEYS", "")
             self.client.sys.submit_unseal_keys(
                 keys.split(",")
             )
 
         if self.client.is_authenticated() is False:
-            self.client.token = worker_ctx.container.config.\
+            self.client.token = self.container.config.\
                 get("VAULT_TOKEN", "")
 
-        return self.client.secrets.kv.read_secret_version
+    def get_kv_secret(self, mount_point="", path=""):
+        try:
+            secret = self.client.secrets.kv.\
+                read_secret_version(mount_point=mount_point, path=path)
+            return secret["data"]
+        except Exception:
+            return None
+
+    def get_dependency(self, worker_ctx):
+        return self
