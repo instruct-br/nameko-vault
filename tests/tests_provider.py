d20ba76a82f10320f8f9b209aa92af9d00bb17b7
Lucas Souto <lucas.souto@instruct.com.br>
2020-09-09 23:47:42 -0300

    tests(provider): Add tests create and patch method

---
+++ tests/tests_provider.py
@@ -0,0 +1,76 @@
+from nameko.rpc import rpc
+from nameko.testing.services import worker_factory
+
+from nameko_vault import VaultProvider
+
+
+class TestVaultService:
+    name = "test_vault_service"
+    vault = VaultProvider(mount_point="test")
+
+    @rpc
+    def create_secret(self, path, secret):
+        response = self.vault.create_or_update_kv_secret(path, secret)
+
+        return response
+
+    @rpc
+    def patch_secret(self, path, secret):
+        response = self.vault.patch_kv_secret(path, secret)
+
+        return response
+
+
+def test_vault_service_create_secret():
+    service = worker_factory(TestVaultService)
+
+    path = "path/test"
+    secret = {"example": "Test", "number": 42}
+    mock_return = {
+        'request_id': '4ce62ee7-0f88-3efc-d745-5e2fbc423789',
+        'lease_id': '',
+        'renewable': False,
+        'lease_duration': 0,
+        'data': {
+            'created_time': '2020-09-10T00:25:40.92411625Z',
+            'deletion_time': '',
+            'destroyed': False,
+            'version': 1,
+        },
+        'wrap_info': None,
+        'warnings': None,
+        'auth': None,
+    }
+
+    service.vault.create_or_update_kv_secret.return_value = mock_return
+
+    assert service.create_secret(path, secret) == mock_return
+    service.vault.create_or_update_kv_secret.assert_called_once_with(
+        path, secret
+    )
+
+
+def test_vault_service_patch_secret():
+    service = worker_factory(TestVaultService)
+
+    path = "path/test"
+    secret = {"example": "Test Patch"}
+    mock_return = {
+        'request_id': '7bf2a869-dc66-efa2-3679-814ef76fb447',
+        'lease_id': '',
+        'renewable': False,
+        'lease_duration': 0,
+        'data': {
+            'created_time': '2020-09-10T00:31:32.6783082Z',
+            'deletion_time': '',
+            'destroyed': False,
+            'version': 2,
+        },
+        'wrap_info': None,
+        'warnings': None,
+        'auth': None,
+    }
+    service.vault.patch_kv_secret.return_value = mock_return
+
+    assert service.patch_secret(path, secret) == mock_return
+    service.vault.patch_kv_secret.assert_called_once_with(path, secret)
