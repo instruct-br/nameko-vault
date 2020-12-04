3bca361f2872ecc65134021c4cb0516669fea1b3
Felipe Pavan <felipe.pavan@instruct.com.br>
2020-12-04 14:27:15 -0300

    feat(provider): Add método delete
    - Adiciona método para remover o metadata e todas as versões de um secret por meio do path
    - Adiciona teste para este novo método
    - Sobe versão para 0.3.0

--- tests/tests_provider.py
+++ tests/tests_provider.py
@@ -19,6 +19,12 @@ class TestVaultService:
         response = self.vault.patch_kv_secret(path, secret)
 
         return response
+    
+    @rpc
+    def delete_metadata_and_all_versions_secret(self, path):
+        response = self.vault.delete_metadata_and_all_versions_kv_secret(path)
+
+        return response
 
 
 def test_vault_service_create_secret():
@@ -74,3 +80,14 @@ def test_vault_service_patch_secret():
 
     assert service.patch_secret(path, secret) == mock_return
     service.vault.patch_kv_secret.assert_called_once_with(path, secret)
+
+
+def test_vault_service_delete_metadata_and_all_versions():
+    service = worker_factory(TestVaultService)
+
+    path = "path/test"
+    mock_return = 204
+    service.vault.delete_metadata_and_all_versions_kv_secret.return_value = mock_return
+
+    assert service.delete_metadata_and_all_versions_secret(path) == mock_return
+    service.vault.delete_metadata_and_all_versions_kv_secret.assert_called_once_with(path)
